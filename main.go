00d18da87e9e7762327599ccde2a3cdcc8650cb7
chenglong <5456@qq.com>
2021-02-03 09:11:04 +0800

    添加路由

--- main.go
+++ main.go
@@ -3,8 +3,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", Redirect)
+	http.HandleFunc("/add", Add)
+	log.Println("http server listening on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Add")
+	fmt.Fprint(w, "Add")
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Redirect")
+	fmt.Fprint(w, "Redirect")
 }
