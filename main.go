e7a3a868e7a1b4edc664fc9ec6b6658b2c87606d
chenglong <5456@qq.com>
2021-02-05 16:00:16 +0800

    添加命令行参数

--- main.go
+++ main.go
@@ -2,17 +2,29 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"goto/got"
 	"log"
 	"net/http"
 )
 
+var port int
+var master string
+
+func init() {
+	flag.IntVar(&port, "port", 8000, "listening port")
+	flag.StringVar(&master, "master", "", "master's address")
+	flag.Parse()
+}
+
 func main() {
 	http.HandleFunc("/", got.Redirect)
 	http.HandleFunc("/add", got.Add)
 	http.HandleFunc("/show", got.Show)
-	log.Println("http server listening on :8000")
-	err := http.ListenAndServe(":8000", nil)
+	addr := fmt.Sprintf(":%d", port)
+	log.Println("http server listening on " + addr)
+	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
