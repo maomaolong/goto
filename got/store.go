ee7c9d2945dfe6a9c854befddb71702d456dde86
chenglong <76677@qq.com>
2021-02-05 14:32:33 +0800

    保存到json文件中

--- got/store.go
+++ got/store.go
@@ -1,8 +1,12 @@
 package got
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"strconv"
 	"sync"
 	"sync/atomic"
@@ -10,10 +14,21 @@ import (
 
 type UrlMap map[string]string
 
+type record struct {
+	Key, Url string
+}
+
 type Store struct {
+	file *os.File
 	rw   sync.RWMutex
 	key  int64
 	urls UrlMap
+}
+
+func (s *Store) Set(key, url string) {
+	s.rw.Lock()
+	defer s.rw.Unlock()
+	s.urls[key] = url
 }
 
 func (s *Store) Add(url string) (string, error) {
@@ -25,6 +40,10 @@ func (s *Store) Add(url string) (string, error) {
 		return "", errors.New(key + "is exist")
 	}
 	s.urls[key] = url
+	err := s.save(key, url)
+	if err != nil {
+		log.Println(err.Error())
+	}
 	return fmt.Sprintf("http://localhost:8000/%s", key), nil
 }
 
@@ -43,8 +62,39 @@ func (s *Store) createKey() string {
 	return strconv.FormatInt(s.key, 10)
 }
 
+func (s *Store) load() error {
+	decoder := json.NewDecoder(s.file)
+	var err error
+	for err == nil {
+		var r record
+		if err = decoder.Decode(&r); err == nil {
+			s.key = r.Key
+			s.Set(r.Key, r.Url)
+		}
+	}
+	if err == io.EOF {
+		return nil
+	}
+	return err
+}
+
+func (s *Store) save(key, url string) error {
+	encoder := json.NewEncoder(s.file)
+	return encoder.Encode(record{key, url})
+}
+
 func NewStore() *Store {
-	return &Store{
+	store := &Store{
 		urls: make(UrlMap),
 	}
+	f, err := os.OpenFile("store.json", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	store.file = f
+	err = store.load()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return store
 }
